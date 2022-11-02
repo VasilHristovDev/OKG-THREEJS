bfb9e35c8e9525760f2ff84ab3df5203070a28ce
Vasil Hristov <vasil.hristov@netea.bg>
2022-11-02 22:37:42 +0200

    [moidom] added fireplace logic

--- moidom/bed.js
+++ moidom/bed.js
@@ -4,14 +4,34 @@ class Bed {
   panels;
   color;
   pillowColor;
+  blanketColor;
 
-  constructor(width, height, panels, color, pillowColor) {
+  constructor(width, height, panels, color, pillowColor, blanketColor) {
     this.width = width;
     this.height = height;
     this.panels = panels;
     this.color = color;
     this.pillowColor = pillowColor;
+    this.blanketColor = blanketColor;
   }
+
+  getPillow(geometry, material, index) {
+    const pillowObj = new THREE.Mesh(geometry, material);
+    pillowObj.rotateX(Math.PI / 2);
+    pillowObj.rotateZ(Math.PI / 2);
+    pillowObj.position.y = 0.21;
+    pillowObj.position.x = this.width / 2 - 0.1;
+    pillowObj.position.z = (-this.height / 2 + (this.height / this.panels * (index + 1))) - 0.6;
+
+    return pillowObj;
+  }
+
+  getBlanket(geometry, material, index) {
+    const blanketObj = new THREE.Mesh(geometry, material);
+    blanketObj.position.set(-this.width/2 + 0.3, 0.1, -this.height / 2 + (this.height / this.panels * (index + 1)) - 0.5)
+    return blanketObj;
+  }
+
 
   getObject() {
     const foundation = new THREE.BoxBufferGeometry(this.width, this.height, 0.1);
@@ -27,43 +47,49 @@ class Bed {
       bevelOffset: 0,
       bevelSegments: 1
     };
-    pillow.moveTo( 0,0 );
-    pillow.lineTo( 0, 0.2 );
-    pillow.lineTo( 0.3, 0.2 );
-    pillow.lineTo( 0.3, 0 );
-    pillow.lineTo( 0, 0 );
-
-    const geometry = new THREE.ExtrudeBufferGeometry( pillow, extrudeSettings );
-    const material = new THREE.MeshPhongMaterial( { color: this.pillowColor } );
-    const pillowObj = new THREE.Mesh( geometry, material ) ;
+    pillow.moveTo(0, 0);
+    pillow.lineTo(0, 0.2);
+    pillow.lineTo(0.3, 0.2);
+    pillow.lineTo(0.3, 0);
+    pillow.lineTo(0, 0);
 
     const texture = new THREE.TextureLoader();
-    const foundationMaterial = new THREE.MeshPhongMaterial({color: this.color, map: texture.load("../materials/redcloth.jpg")});
+    const foundationMaterial = new THREE.MeshPhongMaterial({
+      color: this.color,
+      map: texture.load("../materials/bed.jpg")
+    });
     const legMaterial = new THREE.MeshPhongMaterial({map: texture.load("../materials/wood.jpg")});
+    const pillowGeometry = new THREE.ExtrudeBufferGeometry(pillow, extrudeSettings);
+    const pillowMaterial = new THREE.MeshPhongMaterial({color: this.pillowColor, map: texture.load("../materials/pillow.jpg")});
+
+    const blanketGeometry = new THREE.BoxBufferGeometry(0.5, 0.05, 0.7);
+    const blanketMaterial = new THREE.MeshPhongMaterial({color: this.blanketColor, map: texture.load("../materials/blanket.jpg")})
 
     const foundationMattressObj = new THREE.Mesh(foundation, foundationMaterial);
-    const foundationWood = new THREE.Mesh(new THREE.BoxBufferGeometry(this.width,this.height, 0.05), legMaterial);
+    const foundationWood = new THREE.Mesh(new THREE.BoxBufferGeometry(this.width, this.height, 0.05), legMaterial);
+
     const legTopLeft = new THREE.Mesh(leg, legMaterial);
     const legTopRight = new THREE.Mesh(leg, legMaterial);
     const legBottomLeft = new THREE.Mesh(leg, legMaterial);
     const legBottomRight = new THREE.Mesh(leg, legMaterial);
 
-    legBottomRight.position.set(this.width/2 - 0.05, -0.2, this.height/2 - 0.05);
-    legBottomLeft.position.set(-this.width/2 + 0.05, -0.2, this.height/2 - 0.05);
-    legTopLeft.position.set(-this.width/2 + 0.05, -0.2, -this.height/2 + 0.05);
-    legTopRight.position.set(this.width/2 - 0.05, -0.2, -this.height/2 + 0.05);
+    legBottomRight.position.set(this.width / 2 - 0.05, -0.2, this.height / 2 - 0.05);
+    legBottomLeft.position.set(-this.width / 2 + 0.05, -0.2, this.height / 2 - 0.05);
+    legTopLeft.position.set(-this.width / 2 + 0.05, -0.2, -this.height / 2 + 0.05);
+    legTopRight.position.set(this.width / 2 - 0.05, -0.2, -this.height / 2 + 0.05);
     foundationWood.position.y = -0.075;
-    foundationMattressObj.rotateX(Math.PI/2);
-    foundationWood.rotateX(Math.PI/2);
-    pillowObj.rotateX(Math.PI/2);
-    pillowObj.rotateZ(Math.PI/2);
-    pillowObj.position.y = 0.21;
-    pillowObj.position.x = this.width/2 - 0.1;
-    pillowObj.position.z = -0.2;
+    foundationMattressObj.rotateX(Math.PI / 2);
+    foundationWood.rotateX(Math.PI / 2);
 
     const bedGroup = new THREE.Group();
-    bedGroup.add(foundationMattressObj).add(foundationWood).add(pillowObj).add(legTopRight).add(legTopLeft).add(legBottomLeft).add(legBottomRight);
+    bedGroup.add(foundationMattressObj).add(foundationWood).add(legTopRight).add(legTopLeft).add(legBottomLeft).add(legBottomRight);
 
+    for (let i = 0; i < this.panels; i++) {
+      const pillowObj = this.getPillow(pillowGeometry, pillowMaterial, i);
+      const blanketObj = this.getBlanket(blanketGeometry, blanketMaterial, i);
+      bedGroup.add(pillowObj);
+      bedGroup.add(blanketObj);
+    }
     return bedGroup;
   }
 }
---
+++ moidom/fireplace.js
@@ -0,0 +1,27 @@
+class Fireplace {
+  width;
+  height;
+
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getObject() {
+    const foundation = new THREE.BoxBufferGeometry(this.width, this.height/3, this.width);
+    let connector = new THREE.ConeGeometry(this.width * Math.sqrt(2)/2, this.height/3, 4);
+    const chimney = new THREE.BoxBufferGeometry(this.width/2, this.height/3 + 0.3, 0.4);
+
+    const textureLoader = new THREE.TextureLoader();
+    const material = new THREE.MeshPhongMaterial({map:textureLoader.load("../materials/fireplace.jpg")});;
+
+    const foundationObj = new THREE.Mesh(foundation, material);
+    const connectorObj = new THREE.Mesh(connector, material);
+    const chimneyObj = new THREE.Mesh(chimney, material);
+
+    foundationObj.position.set(0,-this.height/3,0);
+    chimneyObj.position.set(0,this.height/3- 0.3,0);
+    connectorObj.rotateY(Math.PI/4);
+    return new THREE.Group().add(foundationObj).add(connectorObj).add(chimneyObj);
+  }
+}
